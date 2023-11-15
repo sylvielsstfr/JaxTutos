315c9f0affe160836e580d6168bdfc00a9cb5a46
sylvielsstfr <dagoret@lal.in2p3.fr>
2023-11-15 08:57:06 +0100

    update tuto

--- pathinit.py
+++ pathinit.py
@@ -1,17 +1,17 @@
 import os
 import sys
 
-rootPath_to_JaxTutos_localrepository = '/pbs/throng/lsst/users/dagoret/desc/TUTORIALS/'
-rootPath_to_JaxTutos_conda_env = '/pbs/throng/lsst/users/dagoret/my_anacondas/miniconda/envs/'
-homePath = os.environ.get("HOME")
+#rootPath_to_JaxTutos_localrepository = '/pbs/throng/lsst/users/dagoret/desc/TUTORIALS/'
+#rootPath_to_JaxTutos_conda_env = '/pbs/throng/lsst/users/dagoret/my_anacondas/miniconda/envs/'
+#homePath = os.environ.get("HOME")
 
-sys.path = [rootPath_to_JaxTutos_localrepository + "JaxTutos", '',               
-            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python38.zip", 
-            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8", 
-            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8/lib-dynload", 
-            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8/site-packages", 
-            homePath+ "/.local/lib/python3.8/site-packages",
-            '/opt/conda/lib/python3.8/site-packages'
-           ]
+#sys.path = [rootPath_to_JaxTutos_localrepository + "JaxTutos", '',               
+#            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python38.zip", 
+#            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8", 
+#            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8/lib-dynload", 
+#            rootPath_to_JaxTutos_conda_env + "JaxTutos/lib/python3.8/site-packages", 
+#            homePath+ "/.local/lib/python3.8/site-packages",
+#            '/opt/conda/lib/python3.8/site-packages'
+#           ]
 
 
